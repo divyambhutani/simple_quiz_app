91efa94ab15422e2c2d653a7dda1b198ed624c96
Divyam <divyambhutani40@gmail.com>
2021-04-04 19:24:42 +0530

    Added list of map of question and answers and their scores.. also added spread operator rather than manually creating options

--- lib/answers.dart
+++ lib/answers.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  final Function pressHandler;
+  final String answer;
+
+  Answer(this.answer, this.pressHandler);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: EdgeInsets.only(top: 10),
       color: Colors.blue,
       child: ElevatedButton(
-        child: Text("anwer"),
-        onPressed: null,
+        child: Text(answer),
+        onPressed: pressHandler,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  void _answerQuestion() {
+  int _totalScore = 0;
+
+  void _answerQuestion(int score) {
     print('Answer Chosen');
     setState(() {
       _questionIndex += 1;
     });
     print(_questionIndex);
+    _totalScore += score;
   }
 
   int _questionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    var questionList = ['Question1', 'Question2', 'Question3'];
+    var questionList = [
+      {
+        'questionText': 'What is your favorite animal?',
+        'answer': [
+          {'text': 'Lion', 'score': 5},
+          {'text': 'Dog', 'score': 7},
+          {'text': 'Cow', 'score': 9},
+          {'text': 'Wolf', 'score': 2},
+        ]
+      },
+      {
+        'questionText': 'What would you like to eat',
+        'answer': [
+          {'text': 'Pasta', 'score': 7},
+          {'text': 'Burger', 'score': 5},
+          {'text': 'Noodles', 'score': 8},
+          {'text': 'Pizza', 'score': 3},
+        ]
+      },
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("This is my first web app"),
         ),
-        body: Column(
-          children: [
-            Questions(questionList.elementAt(_questionIndex)),
-            Answer(),
-            Answer(),
-            Answer(),
-          ],
-        ),
+        body: _questionIndex < questionList.length
+            ? Column(
+                children: [
+                  Questions(
+                      questionList.elementAt(_questionIndex)['questionText']),
+                  ...(questionList[_questionIndex]['answer']
+                          as List<Map<String, Object>>)
+                      .map((answer) {
+                    return Answer(
+                      answer['text'],
+                      () => _answerQuestion(answer['score']),
+                    );
+                  }).toList(),
+                ],
+              )
+            : Container(
+                height: double.infinity,
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Text(
+                  'Your total score is $_totalScore',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
       ),
     );
   }
